8dbf1f5fb6156cffef650974c19388f3b8e75b59
George Miles <gmiles83@gmail.com>
2022-01-16 20:53:52 -0500

    Fixed tag navigation key bug

--- conf/keys.lua
+++ conf/keys.lua
@@ -203,7 +203,7 @@ M.clientkeys = gears.table.join(
 -- Be careful: we use keycodes to make it work on any keyboard layout.
 -- This should map on the top row of your keyboard, usually 1 to 9.
 for i = 1, 9 do
-    globalkeys = gears.table.join(globalkeys,
+    M.globalkeys = gears.table.join(M.globalkeys,
         -- View tag only.
         awful.key({ modkey }, "#" .. i + 9,
                   function ()
